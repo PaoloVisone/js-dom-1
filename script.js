68eb5f12d1f6ca351b4f63ac43d7b9d07b9d4ffc
PaoloVisone <polvis.pv.22@gmail.com>
2025-01-10 16:02:22 +0100

    Add EVENT "CLICK"

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ const luceOn = document.getElementById('accesa')
 const luceOff = document.getElementById('spenta')
 const btnOnOff = document.getElementById('btn')
 
+// il bottone viene cliccato
+btnOnOff.addEventListener("click", ()) => {
+
+});
 
