584cba9de0f50ad4ebe154c64ad6608b3ba756af
PaoloVisone <polvis.pv.22@gmail.com>
2025-01-10 16:54:57 +0100

    Add Function

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // Prendo gli elementi
-const luceOn = document.getElementById('accesa')
-const luceOff = document.getElementById('spenta')
-const btnOnOff = document.getElementById('btn')
+const luceOn = document.getElementById('lampadina-accesa');
+const luceOff = document.getElementById('lampadina-spenta');
+const btnOnOff = document.getElementById('btn');
 
-// il bottone viene cliccato
-btnOnOff.addEventListener("click", ()) => {
 
+// Il bottone viene cliccato
+btnOnOff.addEventListener("click", function () {
+    // L'immagine cambia
+    luceOff.classList = "spenta2";
+    luceOn.classList = "accesa2";
+    luceOn.classList.remove = "accesa";
+
+    // Cambia il testo
+    btnOnOff.innerHTML = 'Spegni';
+
+    // Cambia Colore
+    btnOnOff.classList = "btn-color2"
 });
 
